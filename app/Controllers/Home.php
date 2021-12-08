d9b15f18ae0aa5561e2161c791263efd7167d762
Muhamad Farhan Rajab <farhanrajab15@gmail.com>
2021-12-08 13:51:15 +0700

    first commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        $date = date("Y-m-d");
+        $file = file_get_contents("https://api.pray.zone/v2/times/"
+        ."day.json?city=bogor&date=$date");
+        $data_waktu = json_decode($file, true);
+
+        return view('home');
+    }
+    public function wkwk()
+    {
+        $date = date("Y-m-d");
+        $file = file_get_contents("https://api.pray.zone/v2/times/today.json?city=bogor");
+        $data_waktu = json_encode($file, true);
+
+        return $data_waktu;
+    }
+
+}
