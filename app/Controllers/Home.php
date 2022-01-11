010e6ed62c12ab3a88b89c6ce592186c5f990ab5
Muhamad Farhan Rajab <farhanrajab15@gmail.com>
2022-01-11 09:48:14 +0700

    update tampilan v2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,7 +7,7 @@ class Home extends BaseController
     public function index()
     {
         $date = date("Y-m-d");
-        $file = file_get_contents("https://api.myquran.com/v1/sholat/jadwal/1204/2021/06/23");
+        $file = file_get_contents("https://api.myquran.com/v1/sholat/jadwal/1204/2022/01/15");
         $data_waktu = json_decode($file, true);
         // $data_solat = array('datalist' => $data_waktu );
         $data = $data_waktu['data']['jadwal'];
